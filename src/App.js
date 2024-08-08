237e84eba1db48c07393492f92611b789a7dcada
Bartosz <queker001@gmail.com>
2024-08-08 23:03:01 +0200

    example data

--- src/App.js
+++ src/App.js
@@ -12,12 +12,58 @@ import { Logo } from "./Logo";
 import { ErrorMessage } from "./ErrorMessage";
 import { Loader } from "./Loader";
 
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGFlNDUzYzMtYWNlZC00N2ViLTlhOTAtNGI4NDQ4ZDRkOThkXkEyXkFqcGdeQXVyMzI4MTk3MTY@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
 const KEY = "b8a2cdd0";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [numResult, setNumResult] = useState(null);
@@ -56,6 +102,7 @@ export default function App() {
           setSelectedId("");
           setError("");
           setNumResult(data.Search.length);
+          console.log(movies);
         } catch (err) {
           if (err.message !== "signal is aborted without reason") {
             setError(err.message);
@@ -64,8 +111,8 @@ export default function App() {
           setIsLoading(false);
         }
       }
-      if (query.length < 2) {
-        setMovies([]);
+      if (query.length < 1) {
+        // setMovies([]);
         setSelectedId("");
         setError("");
         return;
